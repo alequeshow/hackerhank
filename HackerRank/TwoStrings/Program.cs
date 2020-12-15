f36fdc301820b34dc43c3254be8ae2366fe6f8af
Alex Almeida <alex.pans@gmail.com>
2020-12-15 22:45:26 +0000

    Add TwoString exercise
    Signed-off-by: Alex Almeida <alex.pans@gmail.com>

---
+++ HackerRank/TwoStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TwoStrings
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                var s1 = Console.ReadLine();
+
+                var s2 = Console.ReadLine();
+
+                var result = TwoStrings.CheckStrings(s1, s2);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ HackerRank/TwoStrings/TwoStrings.cs
@@ -0,0 +1,14 @@
+﻿namespace TwoStrings
+{
+    using System.Linq;
+
+    public static class TwoStrings
+    {
+        public static string CheckStrings(string s1, string s2)
+        {
+            var merge = s1.Intersect(s2);
+
+            return merge.Count() > 0 ? "YES" : "NO";
+        }
+    }
+}
