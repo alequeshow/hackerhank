288629a71f7775b55a582286608dd19cdc506df8
Alex Almeida <alex.pans@gmail.com>
2020-12-15 13:13:59 +0000

    Adding missing files from NewYearChaos
    Signed-off-by: Alex Almeida <alex.pans@gmail.com>

---
+++ HackerRank/NewYearChaos/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NewYearChaos
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                var n = Convert.ToInt32(Console.ReadLine());
+
+                var q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp));
+
+                new QueueControl(q).CheckBribes();
+            }
+        }
+    }
+}
---
+++ HackerRank/NewYearChaos/QueueControl.cs
@@ -0,0 +1,44 @@
+﻿namespace NewYearChaos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class QueueControl
+    {
+        private int[] Queue;
+
+        public QueueControl(int[] queue)
+        {
+            this.Queue = queue;
+        }
+
+        public void CheckBribes()
+        {
+            var orderedQueue = this.Queue.OrderBy(q => q).ToArray();
+            var bribes = 0;
+
+            for (var i = 0; i < this.Queue.Length; i++)
+            {
+                var diff = orderedQueue[i] - this.Queue[i];
+
+                if (diff < -2)
+                {
+                    Console.WriteLine("Too chaotic");
+                    return;
+                }
+                else if (diff < 0)
+                {
+                    bribes += (diff * -1);
+                }
+                else if (i < this.Queue.Length - 1 && this.Queue[i] > this.Queue[i + 1])
+                {
+                    bribes++;
+                }
+            }
+
+            Console.WriteLine(bribes);
+        }
+    }
+}
